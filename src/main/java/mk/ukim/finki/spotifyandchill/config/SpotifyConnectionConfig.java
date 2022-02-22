e8b9dfe66fb128001e59517e33a60bedc3f2948b
Mimziphery <63194172+Mimziphery@users.noreply.github.com>
2022-02-23 00:39:27 +0100

    sucess login and token taken

---
+++ src/main/java/mk/ukim/finki/spotifyandchill/config/SpotifyConnectionConfig.java
@@ -0,0 +1,44 @@
+package mk.ukim.finki.spotifyandchill.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component // to make a default bean
+@Data // to create getter setter
+@JsonIgnoreProperties(ignoreUnknown = true) // to ignore
+@PropertySource("classpath:application.properties") // to read property values
+public class SpotifyConnectionConfig {
+
+    @Value("96faf0542a934748ab800dae8f474e70") // TODO add relevant fields
+    private String clientId;
+
+    @Value("${spotify.client-secret}") // TODO add relevant fields
+    private String clientSecret;
+
+    @Setter(AccessLevel.NONE)
+    private String authorizeUrl = "https://accounts.spotify.com/authorize/";
+
+    @Setter(AccessLevel.NONE)
+    private String authorizeResponseType = "code";
+
+    @Setter(AccessLevel.NONE)
+    // private String authorizationScope = "user-read-private user-read-email streaming app-remote-control playlist-read-collaborative user-modify-playback-state";
+    private String authorizationScope = "user-read-private playlist-read-private playlist-modify-public user-library-read user-read-recently-played streaming app-remote-control";
+
+    @Value("${spotify.authorization.redirectURL}")
+    private String spotifyAuthorizationRedirectURL;
+
+    @Setter(AccessLevel.NONE)
+    private String tokenUrl = "https://accounts.spotify.com/api/token";
+
+    @Setter(AccessLevel.NONE)
+    private String playerUrlRecentlyPlayed = "https://api.spotify.com/v1/me/player/recently-played";
+
+    @Setter(AccessLevel.NONE)
+    private String albumUrl = "https://api.spotify.com/v1/albums/";
+}
