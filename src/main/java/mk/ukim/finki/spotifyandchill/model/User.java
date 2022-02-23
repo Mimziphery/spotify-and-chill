d30b8086043cbf1a9dd247a8b9d9eafd52c439ff
MarkoKrajceski <markokrajceski@gmail.com>
2022-02-23 20:12:01 +0100

    After merge fixes

--- src/main/java/mk/ukim/finki/spotifyandchill/model/User.java
+++ src/main/java/mk/ukim/finki/spotifyandchill/model/User.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.lang.reflect.*;
 import java.util.Date;
 import java.util.List;
 
@@ -16,91 +17,19 @@ public class User {
     private String surname;
     private String email;
     private Date birthDate;
+    private String profilePicture;
+
+    @ManyToOne
     private Artist topArtist;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getBdate() {
-        return birthDate;
-    }
-
-    public void setBdate(Date bdate) {
-        this.birthDate = bdate;
-    }
-
-    public List<User> getMatches() {
-        return matches;
-    }
-
-    public void setMatches(List<User> matches) {
-        this.matches = matches;
-    }
-
-    public List<Artist> getLikedArtist() {
-        return likedArtist;
-    }
-
-    public void setLikedArtist(List<Artist> likedArtist) {
-        this.likedArtist = likedArtist;
-    }
-
-    public List<Song> getLikedSongs() {
-        return likedSongs;
-    }
-
-    public void setLikedSongs(List<Song> likedSongs) {
-        this.likedSongs = likedSongs;
-    }
-
-    private List<User> matches;
-    private List<Artist> likedArtist;
-    private List<Song> likedSongs;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    private List<User> matches;
+//    private List<Artist> likedArtist;
+//
+//    private List<Song> likedSongs;
+//    private Long id;
 
-    @Id
-    public Long getId() {
-        return id;
-    }
 }
