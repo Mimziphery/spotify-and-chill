1521585f825a7ff814e37be6655af3d485be9893
Mimziphery <63194172+Mimziphery@users.noreply.github.com>
2022-02-27 23:11:28 +0100

    About us started and bug about navbar fixed
    the link of bootstrap that is used for navbar was in the welcome html, not in the header fragment, fixed

--- src/main/java/mk/ukim/finki/spotifyandchill/web/controller/WelcomeController.java
+++ src/main/java/mk/ukim/finki/spotifyandchill/web/controller/WelcomeController.java
@@ -15,4 +15,9 @@ public class WelcomeController {
     public String getWelcomePage(){
         return "welcome";
     }
+
+    @GetMapping("/aboutUs")
+    public String getAboutUsPage(){
+        return "aboutUs";
+    }
 }
